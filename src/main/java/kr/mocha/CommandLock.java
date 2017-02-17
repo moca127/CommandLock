8bd0a0dbbfef8229edd4dffedd87f8b81b497eb0
moca127 <dhasuwm0415@naver.com>
2017-02-17 11:17:22 +0900

    update

--- src/main/java/kr/mocha/CommandLock.java
+++ src/main/java/kr/mocha/CommandLock.java
@@ -1,12 +1,14 @@
 package kr.mocha;
 
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.ConsoleCommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 import cn.nukkit.utils.TextFormat;
-import kr.mocha.command.CommandLockCommand;
 
 import java.io.File;
 
@@ -14,16 +16,13 @@ import java.io.File;
  * Created by mocha on 16. 11. 16.
  */
 public class CommandLock extends PluginBase implements Listener{
-    public static CommandLock instance;
     public Config config;
 
     @Override
     public void onEnable() {
-        instance = this;
         getDataFolder().mkdirs();
         config = new Config(getDataFolder()+ File.separator+"config.yml", Config.YAML);
         this.getServer().getPluginManager().registerEvents(this, this);
-        this.getServer().getCommandMap().register("commandlock", new CommandLockCommand());
         super.onEnable();
     }
 
@@ -46,8 +45,49 @@ public class CommandLock extends PluginBase implements Listener{
         });
     }
 
-    public static CommandLock getInstance(){
-        return instance;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(TextFormat.RED + "명령어의 권한이 없습니다.");
+            return false;
+        }
+        else{
+            try{
+                switch (args[0]){
+                    case "add":
+                    case "a":
+                        config.set(arrToStr(args, 1), true);
+                        config.save();
+                        sender.sendMessage(TextFormat.AQUA+"[ 알림 ] "+TextFormat.GRAY+arrToStr(args, 1)+" 명령어가 잠겼습니다.");
+                        return true;
+                    case "del":
+                    case "d":
+                        config.remove(arrToStr(args, 1));
+                        sender.sendMessage(TextFormat.AQUA+"[ 알림 ] "+TextFormat.GRAY+arrToStr(args, 1)+" 명령어의 잠금이 해제되었습니다.");
+                        return true;
+                    case "list":
+                    case "l":
+                        sender.sendMessage("=== Locked Commands ===");
+                        config.getKeys().forEach(s -> sender.sendMessage(TextFormat.GREEN+s));
+                        return true;
+                    default:
+                        return false;
+                }
+            }catch(ArrayIndexOutOfBoundsException e){
+                return false;
+            }
+        }
+    }
+
+    public String arrToStr(String[] args, int start) {
+        String r = "";
+        for(int i = start; i < args.length; i++) {
+            if(i == args.length-1)
+                r += args[i];
+            else
+                r += args[i] + " ";
+        }
+        return r;
     }
 
 }
